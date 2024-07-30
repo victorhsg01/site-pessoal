a5fcc5380c683032c74321e8388a1e6a6f1a050e
victorhsg01 <victor.schroeder.goncalves@escola.pr.gov.br>
2024-07-30 11:43:38 -0300

    site3

---
+++ src/main/java/com/eleodoro/site_pessoal/SitePessoalApplication.java
@@ -0,0 +1,13 @@
+package com.eleodoro.site_pessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitePessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitePessoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eleodoro/site_pessoal/SitePessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleodoro.site_pessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitePessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
